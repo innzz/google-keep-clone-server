d959dbd253ccdeee243a33c3758b6147d97a467f
Inzamam <inzamam.pawaskar10@gmail.com>
2022-09-13 20:58:37 +0530

    Added mongo db atlas url in .env and added .env in .gitignore

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
-const dbUrl = "mongodb+srv://inzamam:zE7zAH8CUD4KzRNw@cluster0.fen3e0i.mongodb.net/google-keep-clone?retryWrites=true&w=majority&wtimeoutMS=5000"
-
 const connectDb = handler => async (req,res)=>{
     console.log(`${dbUrl}/google-keep-clone`)
     if(mongoose.connections[0].readyState){
         return handler(req,res)
     }
     // await mongoose.connect(`${process.env.MONGO_URI}/google-keep-clone`);
-    await mongoose.connect(`${dbUrl}/google-keep-clone`);
+    await mongoose.connect(`${process.env.MONGO_URI}/google-keep-clone`);
     return handler(req,res)
 }
 
