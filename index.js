392e91bc7c74ac2ebf12a2f3d63f50971c28e298
Inzamam <inzamam.pawaskar10@gmail.com>
2022-09-13 19:32:28 +0530

    Added start script in package json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+// https://google-keep-server-deploy.herokuapp.com/
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
