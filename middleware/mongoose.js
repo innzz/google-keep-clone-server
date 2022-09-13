adf1eb0e9530eb0c1948174ca278eb1d9fa738f5
Inzamam <inzamam.pawaskar10@gmail.com>
2022-09-13 20:00:35 +0530

    added procfile

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,8 @@ const connectDb = handler => async (req,res)=>{
     if(mongoose.connections[0].readyState){
         return handler(req,res)
     }
-    await mongoose.connect(`${process.env.MONGO_URI}/google-keep-clone`);
+    await mongoose.connect(`mongodb://localhost:27017/google-keep-clone`);
+    // await mongoose.connect(`${process.env.MONGO_URI}/google-keep-clone`);
     return handler(req,res)
 }
 
